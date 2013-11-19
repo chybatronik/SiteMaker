e17de6fa2095369c73e6370a9cdef8aa7e5c998a
chybatronik <chybatron@gmail.com>
2013-11-19 13:46:17 +0400

    rails_apps_composer: models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SiteMaker::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
